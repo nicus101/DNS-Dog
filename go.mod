69f7699eb980f5b2ea4902d8d9edc8097e0bac50
Piotr SQLek Skólski <biuro@sqlek.org>
2024-09-17 23:01:09 +0200

    modularized ip provider into importable modules
    Co-authored-by: Mateusz Pukowiec <nicus101@gmail.com>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.1
 
 require (
 	github.com/ovh/go-ovh v1.6.0
+	github.com/stretchr/testify v1.9.0
 	golang.org/x/net v0.22.0
 )
 
@@ -11,8 +12,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/steinfletcher/apitest v1.5.16 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/oauth2 v0.18.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
--- pkg/publicip/json_test.go
+++ pkg/publicip/json_test.go
@@ -1,64 +1,73 @@
-package main
+package publicip_test
 
 import (
 	"context"
 	"fmt"
-	"net"
 	"net/http"
 	"net/http/httptest"
+	"net/netip"
 	"testing"
 	"time"
 
+	"github.com/nicus101/godyndns-ovh/pkg/publicip"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetIP_invalidApiUrl(t *testing.T) {
-	_, err := GetIP(context.Context(nil))
+func TestHttpJsonIper_invalidApiUrl(t *testing.T) {
+	_, err := publicip.NewHttpJsonIper(
+		"http://example.org",
+		"not-relevant",
+	).Ip(context.Context(nil))
 	assert.Error(t, err)
 }
 
-func TestGetIP(t *testing.T) {
+func TestHttpJsonIper(t *testing.T) {
 	tests := []struct {
 		name string
+
+		key string
 
 		given  string
 		sleep  time.Duration
 		status int
 
 		expectErr string
-		expectIP  net.IP
+		expectIP  netip.Addr
 	}{{
 		name:     "normal",
+		key:      "query",
 		given:    `{"query":"127.12.1.0"}`,
-		expectIP: net.ParseIP("127.12.1.0"),
+		expectIP: netip.MustParseAddr("127.12.1.0"),
 	}, {
 		name:      "timeout",
+		key:       "query",
 		sleep:     time.Millisecond * 20,
 		given:     `{"query":"127.12.1.0"}`,
 		expectErr: "context deadline exceeded",
 	}, {
 		name:      "malformed-response",
 		given:     `=UwU=`,
-		expectErr: "cannot unmarshal ip",
+		expectErr: "invalid character",
 	}, {
 		name:      "status-500",
 		given:     `{"query":"127.12.1.0"}`,
 		status:    http.StatusInternalServerError,
-		expectErr: "received statuscode",
+		expectErr: "received status code",
 	}, {
 		name:      "status-200-but-no-ip",
+		key:       "kici-kici",
 		given:     `{}`,
-		expectErr: "malformed ip",
+		expectErr: "doesn't have valid key",
+	}, {
+		name:      "status-200-but-malformaed-ip",
+		key:       "ip",
+		given:     `{"ip":"sruwu"}`,
+		expectErr: "unable to parse IP",
 	}}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-
-			oldIpAPiUrl := ipAPiUrl
-			defer func() {
-				ipAPiUrl = oldIpAPiUrl
-			}()
 
 			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 				if test.sleep != 0 {
@@ -70,12 +79,14 @@ func TestGetIP(t *testing.T) {
 				fmt.Fprint(w, test.given)
 			}))
 			defer server.Close()
-			ipAPiUrl = server.URL
 
 			ctx, cancel := context.WithTimeout(context.TODO(), time.Millisecond*10)
 			defer cancel()
 
-			ip, err := GetIP(ctx)
+			ip, err := publicip.NewHttpJsonIper(
+				server.URL,
+				test.key,
+			).Ip(ctx)
 			if test.expectErr != "" {
 				assert.ErrorContains(t, err, test.expectErr)
 				return
